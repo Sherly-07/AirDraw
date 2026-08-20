5530b3552c33a19af0997a40adff88d55c487838
sherlyns138-crypto <sherly.n.s.138@kalvium.community>
2026-08-20 19:07:02 +0530

    Track index fingertip with green circle

--- air_draw.py
+++ air_draw.py
@@ -19,6 +19,20 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, frame = cap.read()
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+
+    result = hand_landmarker.detect(mp_image)
+
+    if result.hand_landmarks:
+        for hand in result.hand_landmarks:
+            pixel_x = int(hand[8].x * frame.shape[1])
+            pixel_y = int(hand[8].y * frame.shape[0])
+
+            cv2.circle(frame, (pixel_x, pixel_y), 10, (255, 0, 0), -1)
+
     cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
